355ec1c9a22766182e802b196dae46b65f43b2e4
EngineerDivyansh <83111965+EngineerDivyansh@users.noreply.github.com>
2024-05-07 09:43:45 +0530

    Add files via upload

--- src/pages/Program_Schedule.jsx
+++ src/pages/Program_Schedule.jsx
@@ -1,72 +1,72 @@
 import React from "react";
-import "../styles/ProgramSchedule.css"; // Import the CSS file
+import styles from "../styles/ProgramSchedule.css"; // Import the CSS file
 
 const Program_Schedule = () => {
   // Data for the tables
 
   return (
     <div>
-      <table>
-        <tr>
-          <th>Event</th>
-          <th>Time</th>
-          <th>Speaker</th>
-          <th>Title of Talk</th>
+      <table className={styles.table}>
+        <tr className={styles.tr}>
+          <th className={styles.th}>Event</th>
+          <th className={styles.th}>Time</th>
+          <th className={styles.th}>Speaker</th>
+          <th className={styles.th}>Title of Talk</th>
         </tr>
-        <tr>
-          <th colSpan={4}>Day 1 Nov 15,2024</th>
+        <tr  className={styles.tr}>
+          <th colSpan={4} className={styles.th}>Day 1 Nov 15,2024</th>
         </tr>
-        <tr>
-          <td>Inauguration</td>
-          <td>2:00 - 2:45 PM</td>
+        <tr  className={styles.tr}>
+          <td className={styles.td}>Inauguration</td>
+          <td className={styles.td}>2:00 - 2:45 PM</td>
         </tr>
-        <tr>
-          <td>High Time</td>
-          <td>2:45 - 3:30 PM</td>
+        <tr  className={styles.tr}>
+          <td className={styles.td}>High Time</td>
+          <td className={styles.td}>2:45 - 3:30 PM</td>
         </tr>
         {/* //season1 */}
-        <tr>
+        <tr  className={styles.tr}>
           <th colSpan={4}>
             Season1 Nov 15,2024 <br />
             "Biomarker Discovery and OMICS Technology in Disease Diagnosis"
           </th>
         </tr>
 
-        <tr>
-          <td>Key Note Address</td>
-          <td>2:00 - 2:45 PM</td>
+        <tr  className={styles.tr}>
+          <td className={styles.td}>Key Note Address</td>
+          <td className={styles.td}>2:00 - 2:45 PM</td>
         </tr>
-        <tr>
-          <td>Pienary Lecture</td>
-          <td>2:45 - 3:30 PM</td>
+        <tr  className={styles.tr}>
+          <td className={styles.td}>Pienary Lecture</td>
+          <td className={styles.td}>2:45 - 3:30 PM</td>
         </tr>
-        <tr>
-          <td>Tea</td>
-          <td>2:00 - 2:45 PM</td>
+        <tr  className={styles.tr}>
+          <td className={styles.td}>Tea</td>
+          <td className={styles.td}>2:00 - 2:45 PM</td>
         </tr>
-        <tr>
-          <td>IL-1</td>
-          <td>2:45 - 3:30 PM</td>
-        </tr>
-
-        <tr>
-          <td>OP-1</td>
-          <td>2:00 - 2:45 PM</td>
-        </tr>
-        <tr>
-          <td>OP-2</td>
-          <td>2:45 - 3:30 PM</td>
+        <tr  className={styles.tr}>
+          <td className={styles.td}>IL-1</td>
+          <td className={styles.td}>2:45 - 3:30 PM</td>
         </tr>
 
-        <tr>
-          <td>OP-3</td>
-          <td>2:00 - 2:45 PM</td>
+        <tr  className={styles.tr}>
+          <td className={styles.td}>OP-1</td>
+          <td className={styles.td}>2:00 - 2:45 PM</td>
         </tr>
-        <tr>
-          <td>
+        <tr  className={styles.tr}>
+          <td className={styles.td}>OP-2</td>
+          <td className={styles.td}>2:45 - 3:30 PM</td>
+        </tr>
+
+        <tr  className={styles.tr}>
+          <td className={styles.td}>OP-3</td>
+          <td className={styles.td}>2:00 - 2:45 PM</td>
+        </tr>
+        <tr  className={styles.tr}>
+          <td className={styles.td}>
             <b>Dinner</b>
           </td>
-          <td>2:45 - 3:30 PM</td>
+          <td className={styles.td}>2:45 - 3:30 PM</td>
         </tr>
       </table>
     </div>
