5c0adcf45bc358e996511089a86b61e2e3e943d6
EngineerDivyansh <83111965+EngineerDivyansh@users.noreply.github.com>
2024-05-07 09:34:10 +0530

    Add files via upload

--- src/pages/Program_Schedule.jsx
+++ src/pages/Program_Schedule.jsx
@@ -1,38 +1,76 @@
-import styles from "../styles/ProgramSchedule.module.css";
+import React from "react";
+import "../styles/ProgramSchedule.css"; // Import the CSS file
 
-function Program_Schedule() {
+const Program_Schedule = () => {
+  // Data for the tables
+
   return (
-    <div className={styles.container}>
-      <h2>Program Schedule</h2>
-      <table className={styles.table}>
-        <thead>
-          <tr>
-            <th>Time</th>
-            <th>Event</th>
-            <th>Speaker</th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr>
-            <td>9:00 AM</td>
-            <td>Registration</td>
-            <td>-</td>
-          </tr>
-          <tr>
-            <td>10:00 AM</td>
-            <td>Opening Ceremony</td>
-            <td>John Doe</td>
-          </tr>
-          <tr>
-            <td>11:00 AM</td>
-            <td>Keynote Speech</td>
-            <td>Jane Smith</td>
-          </tr>
-          {/* Add more rows as needed */}
-        </tbody>
+    <div>
+      <table>
+        <tr>
+          <th>Event</th>
+          <th>Time</th>
+          <th>Speaker</th>
+          <th>Title of Talk</th>
+        </tr>
+        <tr>
+          <th colSpan={4}>Day 1 Nov 15,2024</th>
+        </tr>
+        <tr>
+          <td>Inauguration</td>
+          <td>2:00 - 2:45 PM</td>
+        </tr>
+        <tr>
+          <td>High Time</td>
+          <td>2:45 - 3:30 PM</td>
+        </tr>
+        {/* //season1 */}
+        <tr>
+          <th colSpan={4}>
+            Season1 Nov 15,2024 <br />
+            "Biomarker Discovery and OMICS Technology in Disease Diagnosis"
+          </th>
+        </tr>
+
+        <tr>
+          <td>Key Note Address</td>
+          <td>2:00 - 2:45 PM</td>
+        </tr>
+        <tr>
+          <td>Pienary Lecture</td>
+          <td>2:45 - 3:30 PM</td>
+        </tr>
+        <tr>
+          <td>Tea</td>
+          <td>2:00 - 2:45 PM</td>
+        </tr>
+        <tr>
+          <td>IL-1</td>
+          <td>2:45 - 3:30 PM</td>
+        </tr>
+
+        <tr>
+          <td>OP-1</td>
+          <td>2:00 - 2:45 PM</td>
+        </tr>
+        <tr>
+          <td>OP-2</td>
+          <td>2:45 - 3:30 PM</td>
+        </tr>
+
+        <tr>
+          <td>OP-3</td>
+          <td>2:00 - 2:45 PM</td>
+        </tr>
+        <tr>
+          <td>
+            <b>Dinner</b>
+          </td>
+          <td>2:45 - 3:30 PM</td>
+        </tr>
       </table>
     </div>
   );
-}
+};
 
 export default Program_Schedule;
